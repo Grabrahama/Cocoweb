b0ba74f5ae371be711705831e72825c6becffcc5
Ranon-Grabrahama <Grabrahama@hotmail.com>
2021-04-25 16:37:56 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.Gopher', { delay: 500});
+ScrollReveal().reveal('.about-me', { delay: 500});
+ScrollReveal().reveal('.my-projects', { delay: 500});
+ScrollReveal().reveal('.title', { delay: 500});
+ScrollReveal().reveal('.social', { delay: 500});
